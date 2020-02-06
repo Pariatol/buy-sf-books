2f0da792c7d251d020f19b5f4c34f4dcac566db2
Pariatol <pierre.tromeur@gmail.com>
2020-02-06 22:54:20 +0100

    Updates

---
+++ precache-manifest.ae3dfa671c66a2a6b17462c86a8529b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090a07022b028b0c997b49d8f6f6e957",
+    "url": "/buy-sf-books/index.html"
+  },
+  {
+    "revision": "eed42072428652878798",
+    "url": "/buy-sf-books/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8051ce8bdf6f7f271467",
+    "url": "/buy-sf-books/static/css/main.f16ebe75.chunk.css"
+  },
+  {
+    "revision": "eed42072428652878798",
+    "url": "/buy-sf-books/static/js/2.78a7bbe4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/buy-sf-books/static/js/2.78a7bbe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8051ce8bdf6f7f271467",
+    "url": "/buy-sf-books/static/js/main.f3ed1bc3.chunk.js"
+  },
+  {
+    "revision": "5713b7a6535b7c8c9712",
+    "url": "/buy-sf-books/static/js/runtime-main.83ed29eb.js"
+  },
+  {
+    "revision": "ceca0a1ffddf45b630813e7242fa62f9",
+    "url": "/buy-sf-books/static/media/bg.ceca0a1f.jpg"
+  }
+]);
